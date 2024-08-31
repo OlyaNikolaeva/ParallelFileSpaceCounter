af0de3b94a04010160143eb018e8988e556056f8
olya_nick <olya17nikolbas@gmail.com>
2024-09-01 00:42:31 +0300

    Добалено параллельное подсчитываение пробелов в папке с файлами.

---
+++ ParallelFileSpaceCounter/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+
+namespace ParallelFileSpaceCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь к папке с файлами:");
+            string? folderPath = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(folderPath) || !Directory.Exists(folderPath))
+            {
+                Console.WriteLine("Указан некорректный путь.");
+                return;
+            }
+
+            // Старт
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            // Вычисление количества пробелов в файлах
+            int totalSpaces = CountSpacesInFolder(folderPath).Result;
+
+            // Остановка
+            stopwatch.Stop();
+
+            Console.WriteLine($"Общее количество пробелов: {totalSpaces}");
+            Console.WriteLine($"Время выполнения: {stopwatch.ElapsedMilliseconds} мс");
+        }
+
+        static async Task<int> CountSpacesInFolder(string folderPath)
+        {
+            string[] files = Directory.GetFiles(folderPath);
+
+            // Создание и запуск задач для параллельного чтения файлов и подсчета пробелов
+            Task<int>[] tasks = files.Select(file => Task.Run(() => CountSpacesInFile(file))).ToArray();
+
+            // Ожидание завершения всех задач и суммирование
+            int[] results = await Task.WhenAll(tasks);
+            return results.Sum();
+        }
+
+        static int CountSpacesInFile(string filePath)
+        {
+            // Чтение содержимого файла
+            string content = File.ReadAllText(filePath);
+
+            return content.Count(c => c == ' ');
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ParallelFileSpaceCounter
 An application designed for parallel reading of text files from a user-selected folder and counting the number of spaces in their contents. The program uses the power of multiple tasks using the Task class to speed up file processing.
+
+## Результаты измерения времени выполнения
+
+| Количество файлов | Общее количество пробелов | Время выполнения (мс) |
+|-------------------|---------------------------|-----------------------|
+| 3                 | 144                       | 22                    |
+| 3                 | 743                       | 37                    |
+| 3                 | 688                       | 33                    |
+| 3                 | 3970                      | 119                   |
